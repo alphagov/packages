4bbec62e05a861ae948d8385f2b1394e97f379d0
Paul Battley <pbattley@gmail.com>
2011-10-19 11:05:37 +0100

    Build and update a Debian repository.

---
+++ repo.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+DISTRIBUTIONS="current"
+COMPONENTS="main"
+ARCHITECTURES="all amd64 i386"
+
+for dist in $DISTRIBUTIONS; do
+  for comp in $COMPONENTS; do
+    for arch in $ARCHITECTURES; do
+      path=repo/dists/$dist/$comp/binary-$arch
+      mkdir -p $path
+      cat >$path/Release <<END
+Archive: gds
+Component: $comp
+Origin: Government Digital Service, UK
+Label: GDS Deployment Repository
+Architecture: $arch
+END
+      dpkg-scanpackages $path /dev/null | gzip -9c > $path/Packages.gz
+    done
+  done
+done
