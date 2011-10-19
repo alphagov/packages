dcabeabd128479d7c0b5be6ac266558a4b5c4930
Paul Battley <pbattley@gmail.com>
2011-10-19 16:04:13 +0100

    Put -all packages into a pool directory to avoid uploading twice.

--- repo.sh
+++ repo.sh
@@ -4,8 +4,10 @@ DISTRIBUTIONS="current"
 COMPONENTS="main"
 ARCHITECTURES="amd64 i386"
 
-mkdir -p repo
+mkdir -p repo/pool
 cd repo
+
+cp ../debs/*_all.deb pool/
 
 for dist in $DISTRIBUTIONS; do
   for comp in $COMPONENTS; do
@@ -19,8 +21,11 @@ Origin: Government Digital Service, UK
 Label: GDS Deployment Repository
 Architecture: $arch
 END
-      cp ../debs/*_{all,$arch}.deb $path/ # TODO: Be cleverer about distributions
-      dpkg-scanpackages $path /dev/null | gzip -9c > $path/Packages.gz
+      cp ../debs/*_$arch.deb $path/ # TODO: Be cleverer about distributions
+      dpkg-scanpackages $path /dev/null >  $path/Packages
+      dpkg-scanpackages pool  /dev/null >> $path/Packages
+      gzip -9c < $path/Packages > $path/Packages.gz
+      rm $path/Packages
     done
   done
 done
