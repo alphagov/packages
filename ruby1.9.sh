c70d5f2d8a32a14fa6c968345018519144884c10
Paul Battley <pbattley@gmail.com>
2011-12-09 22:38:04 +0000

    Try Ruby 1.9.3.

--- ruby1.9.sh
+++ ruby1.9.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 set -e
 
+VERSION="1.9.3-p0"
+
 mkdir -p build
 cd build
 
 # requires build-essential libssl-dev libreadline6-dev zlib1g-dev
-wget ftp://ftp.ruby-lang.org//pub/ruby/1.9/ruby-1.9.2-p290.tar.gz
-tar -zxvf ruby-1.9.2-p290.tar.gz
-wget https://raw.github.com/gist/1008945/4edd1e1dcc1f0db52d4816843a9d1e6b60661122/ruby-1.9.2p290.patch
-cd ruby-1.9.2-p290
-patch -p1 < ../ruby-1.9.2p290.patch
-time (./configure --prefix=/usr --with-openssl-dir=/usr --with-readline-dir=/usr --with-zlib-dir=/usr && make && make install DESTDIR=installdir)
+if [ ! -d ruby-${VERSION} ]; then
+  wget http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-${VERSION}.tar.gz
+  tar -zxvf ruby-${VERSION}.tar.gz
+fi
 
-fpm -s dir -t deb -n ruby -v 1.9.2-p290 -C installdir \
+cd ruby-${VERSION}
+
+./configure \
+  --prefix=/usr \
+  --with-openssl-dir=/usr \
+  --with-readline-dir=/usr \
+  --with-zlib-dir=/usr
+make
+make install DESTDIR=installdir
+
+fpm -s dir -t deb -n ruby -v ${VERSION} -C installdir \
   -p ruby-VERSION_ARCH.deb -d "libstdc++6 (>= 4.4.3)" \
   -d "libc6 (>= 2.6)" -d "libffi5 (>= 3.0.4)" -d "libgdbm3 (>= 1.8.3)" \
   -d "libncurses5 (>= 5.7)" -d "libreadline6 (>= 6.1)" \
