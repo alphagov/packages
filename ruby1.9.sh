926ed132547188b28cbdf6591d2d1531d8198c3d
Paul Battley <pbattley@gmail.com>
2011-12-12 12:31:29 +0000

    Re-expose the ruby_current_thread symbol.
    This is needed for perftools.rb, ruby-debug, and possibly other libraries.

--- ruby1.9.sh
+++ ruby1.9.sh
@@ -2,6 +2,7 @@
 set -e
 
 VERSION="1.9.3-p0"
+USER_VERSION="-gds2"
 
 mkdir -p build
 cd build
@@ -14,6 +15,12 @@ fi
 
 cd ruby-${VERSION}
 
+if [ ! -f 47.diff ]; then
+  wget https://github.com/ruby/ruby/pull/47.diff
+fi
+
+patch -p1 --forward < 47.diff || echo
+
 ./configure \
   --prefix=/usr \
   --with-openssl-dir=/usr \
@@ -22,7 +29,7 @@ cd ruby-${VERSION}
 make
 make install DESTDIR=installdir
 
-fpm -s dir -t deb -n ruby -v ${VERSION} -C installdir \
+fpm -s dir -t deb -n ruby -v ${VERSION}${USER_VERSION} -C installdir \
   -p ruby-VERSION_ARCH.deb -d "libstdc++6 (>= 4.4.3)" \
   -d "libc6 (>= 2.6)" -d "libffi5 (>= 3.0.4)" -d "libgdbm3 (>= 1.8.3)" \
   -d "libncurses5 (>= 5.7)" -d "libreadline6 (>= 6.1)" \
