8ceb3031480d0e3d847a2b5318a85dfbe12d32dd
Nick Stenning <nick@whiteink.com>
2012-07-16 13:51:39 +0100

    Update node build script for node v0.8

--- node.sh
+++ node.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-VERSION="0.4.12"
+VERSION="0.8.2"
 
 mkdir -p build
 cd build
@@ -15,10 +15,14 @@ time (./configure --prefix=/usr && make && make install DESTDIR=installdir)
 
 fpm -s dir -t deb -n nodejs -v $VERSION -C installdir \
   -p nodejs-VERSION_ARCH.deb \
-  -d "libstdc++6 (>= 4.4.3)" \
+  -d "libc-ares2 (>= 1.7.3)" \
   -d "libc6 (>= 2.6)" \
-  -d "libssl0.9.8 (>= 0.9.8)" \
-  -d "zlib1g (>= 1:1.2.2)" \
+  -d "libev4 (>= 1:4.04)" \
+  -d "libgcc1 (>= 1:4.1.1)" \
+  -d "libssl1.0.0 (>= 1.0.1)" \
+  -d "libstdc++6 (>= 4.1.1)" \
+  -d "libv8-3.8.9.20" \
+  -d "zlib1g (>= 1:1.1.4)" \
   usr/bin usr/lib usr/share/man usr/include
 
 mkdir -p ../../debs
