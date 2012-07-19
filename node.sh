add956d806e0f72dd0ba9391c28fa4c3f34454cf
Sam Sharpe <sam.sharpe@digital.cabinet-office.gov.uk>
2012-07-19 14:35:11 +0100

    * node.sh: Changing deps to match Ubuntu 10.04

--- node.sh
+++ node.sh
@@ -15,13 +15,13 @@ time (./configure --prefix=/usr && make && make install DESTDIR=installdir)
 
 fpm -s dir -t deb -n nodejs -v $VERSION -C installdir \
   -p nodejs-VERSION_ARCH.deb \
-  -d "libc-ares2 (>= 1.7.3)" \
+  -d "libc-ares2 (>= 1.7.0)" \
   -d "libc6 (>= 2.6)" \
-  -d "libev4 (>= 1:4.04)" \
+  -d "libev3 (>= 1:3.8-1)" \
   -d "libgcc1 (>= 1:4.1.1)" \
-  -d "libssl1.0.0 (>= 1.0.1)" \
+  -d "libssl0.9.8 (>= 0.9.8)" \
   -d "libstdc++6 (>= 4.1.1)" \
-  -d "libv8-3.8.9.20" \
+  -d "libv8-2.0.3" \
   -d "zlib1g (>= 1:1.1.4)" \
   usr/bin usr/lib usr/share/man usr/include
 
