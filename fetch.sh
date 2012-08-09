a0717032a43da3fe9f77d9e74c587400b98f65ea
Nick Stenning <nick@whiteink.com>
2012-08-09 13:13:47 +0100

    Don't redownload everything ever every time

--- fetch.sh
+++ fetch.sh
@@ -1,9 +1,3 @@
-#!/bin/bash
-# Fetch existing debs from S3 into debs/ without overwriting local ones.
-
+#!/bin/sh
 set -e
-mkdir -p debs.new
-s3cmd -c s3cfg-gds sync s3://gds-packages/pool/ debs.new/
-cp debs/* debs.new
-rm -rf debs
-mv debs.new debs
+s3cmd -c s3cfg-gds sync s3://gds-packages/pool/ debs/
